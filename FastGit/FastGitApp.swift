36e72c91a53ca27ad47f370f58f7a0929f2cade8
Zhu Chen <stephan.zhuchen@gmail.com>
2025-08-23 20:37:17 +0800

    Initial Commit

---
+++ FastGit/FastGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastGitApp.swift
+//  FastGit
+//
+//  Created by 朱晨 on 2025/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FastGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
